8b93d59f8df8a52375ac09f7ca69a58089a57435
Ananya Singh <singhananya5040@gmail.com>
2025-06-16 10:43:24 +0530

    16-06-25

--- SummerPEP/findDuplicate.cpp
+++ SummerPEP/findDuplicate.cpp
@@ -1,35 +1,30 @@
 // Find duplicates har element ek baari, find duplicate
 #include <bits/stdc++.h>
 using namespace std;
-int findDuplicate(int arr[],int size)
+int findDuplicate(int arr[], int size)
 {
-    int ans = 0;
-    unordered_map<int,int> mpp;
-    // pehla index dusra number of times
-    for(int i = 0;i<size;i++)
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < size; i++)
     {
-        mpp[arr[i]]++;
+        if (mpp.count(arr[i]))
+        {
+            return arr[i];
+        }
+      mpp[arr[i]]++;
     }
-    for(auto it : mpp)
-    {
-        if(it.second>1)
-        ans = it.first;
-    }
-    return ans;
-
+    return -1;
 }
 int main()
 {
     int arr[100];
     int size;
-   cout<<"Enter size: "<<endl;
-   cin>>size;
-   cout<<"Enter elements: "<<endl;
-   for(int i =0;i<size;i++)
-   {
-    cin>>arr[i];
-   }
-   cout<<"Duplicate Element: "<<findDuplicate(arr,size);
-    return 0;
+    cout << "Enter size: " << endl;
+    cin >> size;
+    cout << "Enter elements: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Duplicate Element: " << findDuplicate(arr, size);
     return 0;
 }
