633a8a13baafe138e8416001becd380262f565e5
Ananya Singh <singhananya5040@gmail.com>
2025-06-16 10:33:32 +0530

    16-06-25

--- SummerPEP/array1.cpp
+++ SummerPEP/array1.cpp
@@ -1,11 +1,12 @@
 // Given an array of size N , range 0 to N inclusive, find the missing element
 #include <bits/stdc++.h>
+
 using namespace std;
 int findMissingElement(int arr[],int size)
 {
     int sum=0;
     int actual = (size* (size+1))/2;
-    for(int i =0;i<=size;i++)
+    for(int i =0;i<size;i++)
     {
         sum+=arr[i];
     }
@@ -23,6 +24,6 @@ int main()
    {
     cin>>arr[i];
    }
-   cout<<findMissingElement(arr,size);
+   cout<<"Missing Element: "<<findMissingElement(arr,size);
     return 0;
 }
