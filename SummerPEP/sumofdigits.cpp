a99cc7cccf31a21d9206cb11cd0092b2d4b6a1ab
Ananya Singh <singhananya5040@gmail.com>
2025-06-18 10:14:49 +0530

    18-06-25

--- SummerPEP/sumofdigits.cpp
+++ SummerPEP/sumofdigits.cpp
@@ -13,6 +13,14 @@ int sumOfDigits(int n) {
     }
 int main()
 {
-    
+     int arr[100];
+    int size;
+    cout << "Enter size: " << endl;
+    cin >> size;
+    cout << "Enter elements: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
     return 0;
 }
