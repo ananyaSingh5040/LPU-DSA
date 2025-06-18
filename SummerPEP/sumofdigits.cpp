3b60f261d44791ccf7986ce32139210b6cecea80
Ananya Singh <singhananya5040@gmail.com>
2025-06-18 10:14:27 +0530

    18-06-25

---
+++ SummerPEP/sumofdigits.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sumOfDigits(int n) {
+        
+        int sum = 0;
+        while(n>0)
+        {
+            sum+=n%10;
+            n= n/10;
+        }
+        return sum;
+        
+    }
+int main()
+{
+    
+    return 0;
+}
