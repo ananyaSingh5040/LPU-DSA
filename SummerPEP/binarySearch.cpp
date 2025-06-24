387e1a7b18ff15107018b07b84c4cdb7b0b4d4c7
Ananya Singh <singhananya5040@gmail.com>
2025-06-24 09:32:52 +0530

    24-06-25

---
+++ SummerPEP/binarySearch.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int binarySearch (int n, int target)
+{
+    int start = 0;
+    int end = n - 1;
+    int ans = 0;
+    while(start < end)
+    {
+        int mid = start + (end - start) / 2;
+        if(mid < target)
+    {
+        start = mid + 1;
+    }
+    else if(mid > target)
+    {
+        end = mid;
+    }
+    else
+    {
+        ans =  mid;
+        break;
+    }
+    }
+    return ans;
+}
+int main()
+{
+    int n, target ;
+    cout<<"Enter n: ";
+    cin>>n;
+    cout<<"Enter target: ";
+    cin>>target;
+    cout<<"Missing number is: "<<binarySearch(n,target);
+    return 0;
+}
