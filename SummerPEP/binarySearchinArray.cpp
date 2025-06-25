87fc5caa35afe845c2b08a5e3230b775cb6ad878
Ananya Singh <singhananya5040@gmail.com>
2025-06-25 09:10:24 +0530

    25-06-25

--- SummerPEP/binarySearchinArray.cpp
+++ SummerPEP/binarySearchinArray.cpp
@@ -6,7 +6,7 @@ int binarySearchInArray(vector<int> arr, int target)
     int start = 0;
     int end = n - 1;
     int targetIndex = -1;
-    while (start < end)
+    while (start <= end)
     {
         int mid = start + (end - start) / 2;
         if (arr[mid] < target)
