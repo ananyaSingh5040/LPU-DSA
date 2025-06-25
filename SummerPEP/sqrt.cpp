e6dd329f39fd28822fd474316fe670fb43c9ebb4
Ananya Singh <singhananya5040@gmail.com>
2025-06-25 09:11:09 +0530

    25-06-25

---
+++ SummerPEP/sqrt.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int squareRootInt(int n)
+{
+    int ans = INT_MAX;
+    
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<squareRootInt(n);
+    return 0;
+}
