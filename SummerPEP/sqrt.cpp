a553492c16db209dabb5e3aebf53e82a1ef0d1dc
Ananya Singh <singhananya5040@gmail.com>
2025-06-25 09:28:54 +0530

    25-06-25

--- SummerPEP/sqrt.cpp
+++ SummerPEP/sqrt.cpp
@@ -3,16 +3,38 @@ using namespace std;
 int squareRootInt(int n)
 {
     int ans = 0;
-    for(int i = 1; i <= n/2; i++)
+    // for(int i = 1; i <= n/2; i++)
+    // {
+    //     if(i*i <= n)
+    //     {
+    //         ans = i;
+    //     }
+    //     else
+    //     {
+    //         break;
+    //     }
+    // }
+    int low = 0;
+    int high = n - 1;
+    while(low <= high)
     {
-        if(i*i <= n)
+        int mid = low + (high - low)/2;
+        int sq = mid * mid;
+        if(sq == n)
         {
-            ans = i;
+            ans = mid;
+            break;
+        }
+        else if (sq < n)
+        {
+            ans = mid;
+            low = mid + 1;
         }
         else
         {
-            break;
+            high = mid - 1;
         }
+
     }
     return ans;
 }
