e4045a2c8706fd1461d6c7a063d1b0b33391f2e7
Ananya Singh <singhananya5040@gmail.com>
2025-07-02 09:52:22 +0530

    02-07-2025

--- SummerPEP/quickSort.cpp
+++ SummerPEP/quickSort.cpp
@@ -1,4 +1,4 @@
-// All the elements before the pivot is smaller than two
+// All the elements before the pivot are smaller than pivot and all the elements after the pivot are greater than pivot.
 #include <bits/stdc++.h>
 using namespace std;
 void quickSort (vector<int> &arr)
