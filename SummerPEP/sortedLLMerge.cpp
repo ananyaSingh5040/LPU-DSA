8de961b0e754c333a33170a2a3ef6965dc4152c9
Ananya Singh <singhananya5040@gmail.com>
2025-07-07 09:36:41 +0530

    07-07-25

---
+++ SummerPEP/sortedLLMerge.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        this->data = val;
+        this->next = NULL;
+    }
+};
+Node *addANode(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return head;
+    }
+    Node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = newNode;
+    return head;
+}
+void sortedLL(Node *&head1, Node *&head2)
+{
+    Node *temp1 = head1;
+    Node *temp2 = head2;
+    Node* tracker1 = head1;
+    Node* tracker2 = head2;
+    while (temp1 != NULL && temp2 != NULL)
+    {
+        if(tracker1->data < tracker2->data)
+        {
+            temp1 = temp1->next;
+        }
+        else{
+            tracker1->next = tracker2;
+            temp2->next = temp1;
+        
+        }
+        temp2 = temp2->next;
+    }
+}
+
+void traversal(Node *&head)
+{
+
+    Node *curr = head;
+    while (curr != NULL)
+    {
+        cout << curr->data << " -> ";
+        curr = curr->next;
+    }
+    cout << "NULL";
+}
+int main()
+{
+    // Node* newNode = new Node(10);
+    // Node* head = newNode;
+    Node *head = NULL;
+    addANode(head, 20);
+    addANode(head, 30);
+    traversal(head);
+    return 0;
+}
