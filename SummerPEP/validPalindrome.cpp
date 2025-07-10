74a62fa5bc9fa3a07b6d3199110c3bf22da3f557
Ananya Singh <singhananya5040@gmail.com>
2025-07-10 09:26:30 +0530

    10-07-25

---
+++ SummerPEP/validPalindrome.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void isPalindrome(string s) {
+
+
+        for(char ch: s)
+        {
+            if(s[ch] >= 'A' && s[ch] <='Z')
+            {
+                s[ch] = s[ch] - 'a';
+                cout<<s[ch];
+            }
+        }
+        
+        
+    }
+int main()
+{
+    string s = "AmanaplanacanalPanama";
+    isPalindrome(s);
+    return 0;
+}
