4a5f2b89ed24027db2f7b4c4453aea7671d9fc22
Ananya Singh <singhananya5040@gmail.com>
2025-07-23 14:35:14 +0530

    23-07-25

---
+++ SummerPEP/Arrays/maxElement.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxElement(int arr[][100], int row, int col)
+{
+    int maxi = INT_MIN;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            maxi = max(maxi, arr[i][j]);
+        }
+    }
+    return maxi;
+}
+int main()
+{
+    int row, col;
+    cout << "Enter row size: " << endl;
+    cin >> row;
+    cout << "Enter col size: " << endl;
+    cin >> col;
+    int arr[row][100];
+   cout<<"Enter Elements: "<<endl;
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"The largest of all : "<<maxElement(arr,row,col);
+
+    return 0;
+}
