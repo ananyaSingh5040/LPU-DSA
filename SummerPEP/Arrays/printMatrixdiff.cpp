f3ce2eedb583f280386efa35263334f45a42c245
Ananya Singh <singhananya5040@gmail.com>
2025-07-26 08:45:32 +0530

    26-07-25

---
+++ SummerPEP/Arrays/printMatrixdiff.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printMatrix( int arr[][100], int row, int col)
+{
+    for(int i = 0; i < row; i++)
+    {
+       if(i%2==0)
+       {
+        for(int j = 0; j < col; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+       }
+       else
+       {
+        for(int j = col - 1; j >= 0; j--)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+       }
+    }
+}
+
+int main()
+{
+     int row, col;
+    cout << "Enter row size: " << endl;
+    cin >> row;
+    cout << "Enter col size: " << endl;
+    cin >> col;
+    int arr[row][100];
+   cout<<"Enter Elements: "<<endl;
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < col; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    printMatrix(arr,row,col);
+    return 0;
+}
